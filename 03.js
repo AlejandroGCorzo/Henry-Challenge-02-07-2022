156378538b2e3a2ae81e0077b33c75b13fac6387
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-09-25 16:37:54 -0300

    first commit

---
+++ 03.js
@@ -0,0 +1,36 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function sumaTodosPrimos(array) {
+  // La funcion llamada 'sumaTodosPrimos' recibe como argumento un array de enteros.
+  // y debe devolver la suma total entre todos los numeros que sean primos.
+  // Pista: un número primo solo es divisible por sí mismo y por 1
+  // Nota: Los números 0 y 1 NO son considerados números primos
+  // Ej:
+  // sumaTodosPrimos([1, 5, 2, 9, 3, 4, 11]) devuelve 5 + 2 + 3 + 11 = 21
+  // Nota: Podes usar la funcion 'esPrimo' resuelta en la homework JSII.
+
+  // Tu código aca:
+  var x = 0;
+  var y;
+  let p;
+  loop1: for (let i = 0; i < array.length; i++) {
+    y=0;
+    p=2;
+    if (array[i]<0 || array[i] === 0 || array[i] === 1 || array[i] === 4) continue;
+    while (p < array[i]/2) {
+      if( array[i] % p  === 0 ) continue loop1;
+      p = p + 1;
+    }  
+    y = array[i];
+    x = x + y;
+  }
+  return x;  
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = sumaTodosPrimos
